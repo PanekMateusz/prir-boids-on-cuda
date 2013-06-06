e781e3f0ba40acb36fc8ce16409495ba8f63619c
Mateusz Ochtera <spaczn@gmail.com>
2013-06-06 21:13:28 +0200

    Dodaj prędkość i przyśpieszenie boida

--- boids-seq/Boid.cpp
+++ boids-seq/Boid.cpp
@@ -1,5 +1,10 @@
 #include "Boid.h"
 
 Boid::Boid(int x, int y){
+  srand(time(NULL));
   position = Point2D(x,y);
+  int a = rand()%(2*MAX_SPEED)-MAX_SPEED;
+  int b  = rand()%(2*MAX_SPEED)-MAX_SPEED;
+  velocity = Vector2D(a, b);
+  acceleration = Vector2D(0, 0);
 }
--- boids-seq/Boid.h
+++ boids-seq/Boid.h
@@ -2,11 +2,15 @@
 #define BOID_H
 
 #include "Structures.h"
+#include<stdlib.h>
 
-#define RANGE 32;
+#define MAX_SPEED 100
+#define RANGE 32
 
 class Boid{
   Point2D position;
+  Vector2D velocity;
+  Vector2D acceleration;
 
   public:
     Boid(){};
